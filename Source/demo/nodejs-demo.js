cfcca6d8c61b89e8cee8fadb602778ad239c8ea2
Anil Mujagic <anil.mujagic@gmail.com>
2019-12-10 14:07:56 +0100

    Change min fee in examples

--- Source/demo/nodejs-demo.js
+++ Source/demo/nodejs-demo.js
@@ -4,7 +4,7 @@ var networkCode = 'UNIT_TESTS';
 var wallet = ownBlockchainSdk.crypto.generateWallet();
 console.log('WALLET:', wallet);
 
-var tx = ownBlockchainSdk.transactions.createTx(wallet.address, 1, 0.001);
+var tx = ownBlockchainSdk.transactions.createTx(wallet.address, 1, 0.1);
 tx.addTransferChxAction('CHexBLbHxh8DqnDcFjLRkaYCneiG9h7XVWq', 1.5);
 console.log('JSON:', tx.toJson(4)); // Pretty-printing with 4 space indentation.
 console.log('SIGNED:', tx.sign(networkCode, wallet.privateKey));
